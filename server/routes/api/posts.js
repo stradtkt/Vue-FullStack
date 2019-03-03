e59081833e7e47123b07d7c17bf37a29e4e23a38
Kevin <stradtkt22@gmail.com>
2019-03-03 14:37:31 -0500

    Added some routes

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -3,8 +3,31 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 // GET
-router.get('/', (req, res) => {
-  res.send("Hello");
+router.get('/', async (req, res) => {
+  const posts = await loadPostCollection();
+  res.send(await posts.find({}).toArray());
 });
 
+router.post('/', async (req, res) => {
+  const posts = await loadPostCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  });
+  res.status(201).send();
+});
 
+router.delete('/:id', async (req, res) => {
+  const posts = await loadPostCollection();
+  await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+  res.status(200).send();
+});
+
+async function loadPostCollection() {
+  const client = await mongodb.MongoClient.connect('mongodb://stradtkt:abcd1234@ds159025.mlab.com:59025/vue_express', {
+    useNewUrlParser: true
+  });
+  return client.db('vue_express').collection('posts');
+}
+
+module.exports = router;
