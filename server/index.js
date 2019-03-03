cab91c253c5e886d9207ea16c43eab60cde04f3b
Kevin <stradtkt22@gmail.com>
2019-03-03 17:28:43 -0500

    Added a couple of posts

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(parser.json());
 app.use(cors());
 
 const posts = require('./routes/api/posts');
-
+app.use('/api/posts', posts);
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
